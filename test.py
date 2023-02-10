76f99805767dc47ff2e624d515297adf4ecd0d53
Nick Ali <nicholas77ali@gmail.com>
2023-02-10 02:28:17 -0500

    test

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
 import chess
-
 board = chess.Board()
 
-board.legal_moves
-
-chess.Move.from_uci("a8a1") in board.legal_moves
-
-board.push_san("e4")
-board.push_san("e5")
-
-print(board)
-print(board)
