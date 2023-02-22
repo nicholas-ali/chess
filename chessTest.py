4a87bf2171d14d4b740ce7cf00739a6d072bf073
Nick Ali <nicholas77ali@gmail.com>
2023-02-22 05:21:41 -0500

    finished safe moves function

--- chessTest.py
+++ chessTest.py
@@ -8,26 +8,25 @@ moves = board.legal_moves
 toMoves = list()
 for move in moves:
     toMoves.append(move.to_square)
-
+moves = list(moves)
 bitMoves = chess.SquareSet(toMoves)
 
 squares = chess.SquareSet()
 
 intersections = list()
 for i in range(1, 7):
-    for sqaure in board.pieces(i, False):
-        squares = squares.union(board.attacks(sqaure))
+    for square in board.pieces(i, False):
+        squares = squares.union(board.attacks(square))
         squares = squares.intersection(bitMoves)
 
 print(toMoves)
 print(list(squares))
 for square in squares:
     print(square)
-    intersections.append(toMoves.index(sqaure))
+    intersections.append(toMoves.index(square))
     toMoves.remove(square)
-count = 0
-for i in range(moves):
-    if i in intersections:
-        moves.pop(i-count)
-        count +=1
+print(moves)
+for move in moves:
+    if move.to_square in squares:
+        moves.remove(move)
 print(moves)
